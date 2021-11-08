507c37ab7438996b8f3e5c77919258887481ad82
LAPTOP-0MCHK0P0\bjorn <bjorn.fontaine@outlook.com>
2021-11-08 19:48:38 +0100

    test

---
+++ Television/Worker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Television
+{
+    public sealed class Worker
+    {
+        private static readonly Lazy<Worker> lazy = new Lazy<Worker>(() => new Worker());
+        public static Worker Instance { get { return lazy.Value; } }
+        public bool TvIsOn { get; set; }
+        BackgroundWorker worker = new BackgroundWorker();
+        private Worker()
+        {
+            worker.DoWork += Worker_DoWork;
+        }
+
+        /// <summary>
+        /// Execute this on when u set TvIsOn to true.
+        /// </summary>
+        public void StartWorking()
+        {
+            worker.RunWorkerAsync();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                // Code To get the required action for your tv.
+            }
+        }
+    }
+}
